314a97710b5166e7f5e19d3d769cf888000e7e1c
lanjudev16 <lanjudev16@gmail.com>
2023-06-06 01:20:13 +0600

    Side bar Ok

--- src/Layouts/Dashboard.jsx
+++ src/Layouts/Dashboard.jsx
@@ -1,36 +1,89 @@
 import { NavLink, Outlet } from "react-router-dom";
-
+import { FaAssistiveListeningSystems, FaBars, FaBeer, FaBell, FaChartBar, FaCloudSunRain, FaCog, FaDelicious, FaDollarSign, FaHome, FaInbox, FaInstalod, FaList, FaPhone, FaPhoneVolume, FaRegStopCircle, FaSoundcloud, FaTextHeight } from "react-icons/fa";
+import './Dashboard.css'
 const Dashboard = () => {
   return (
     <div className="drawer drawer-mobile ">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content">
+      <div className="drawer-content bg-[#25293C]">
         <label
           htmlFor="my-drawer-2"
           className="btn btn-primary drawer-button lg:hidden"
         >
-          Open drawer
+          <FaBars></FaBars>
         </label>
         <Outlet></Outlet>
       </div>
-      <div className="drawer-side bg-[#D1A054]">
+      <div className="drawer-side bg-[#2F3349] scroll-container">
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-        
         <ul className="menu p-4 w-80">
+        <div>
+              <h2 className="text-[#A2A6C1] text-2xl ">
+                <span className="text-[#7367F0]">Clean</span>simple
+              </h2>
+              <h4 className="text-[#A2A6C1] my-4">Admin panel</h4>
+            </div>
           <li>
-            <NavLink to="/dashboard/home">
+            <NavLink className="text-[#A2A6C1]  p-3 bg-transparent p-0 text-lg hover:bg-[#6B62D0] hover:text-[#FFFFFF]">
+              <FaHome></FaHome>Dashboard
             </NavLink>
           </li>
-          
-
-          <div className="divider"></div>
           <li>
+            <NavLink className="text-[#A2A6C1]  p-3 bg-transparent p-0 text-lg hover:bg-[#6B62D0] hover:text-[#FFFFFF]">
+              <FaList></FaList>Listings
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/menu"> Our Menu</NavLink>
+            <NavLink className="text-[#A2A6C1]  p-3 bg-transparent p-0 text-lg hover:bg-[#6B62D0] hover:text-[#FFFFFF]">
+              <FaDelicious></FaDelicious>Orders and Delivery
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/order/salad">Order Food</NavLink>
+            <NavLink className="text-[#A2A6C1]  p-3 bg-transparent p-0 text-lg hover:bg-[#6B62D0] hover:text-[#FFFFFF]">
+              <FaChartBar></FaChartBar>Stats
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="text-[#A2A6C1]  p-3 bg-transparent p-0 text-lg hover:bg-[#6B62D0] hover:text-[#FFFFFF]">
+              <FaDollarSign></FaDollarSign> Finance
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="text-[#A2A6C1]  p-3 bg-transparent p-0 text-lg hover:bg-[#6B62D0] hover:text-[#FFFFFF]">
+            <FaPhoneVolume></FaPhoneVolume> Marketing
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="text-[#A2A6C1]  p-3 bg-transparent p-0 text-lg hover:bg-[#6B62D0] hover:text-[#FFFFFF]">
+            <FaInstalod></FaInstalod> Integration
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="text-[#A2A6C1]  p-3 bg-transparent p-0 text-lg hover:bg-[#6B62D0] hover:text-[#FFFFFF]">
+            <FaRegStopCircle></FaRegStopCircle> Reports
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="text-[#A2A6C1]  p-3 bg-transparent p-0 text-lg hover:bg-[#6B62D0] hover:text-[#FFFFFF]">
+            <FaCog></FaCog> Settings
+            </NavLink>
+          </li>
+          <div className="divider bg-[#484D6E] h-[1px] "></div>
+          <h2 className="text-[#A2A6C1]  p-3 bg-transparent p-0 text-lg">Insights</h2>
+          <li>
+            <NavLink className="text-[#A2A6C1]  p-3 bg-transparent p-0 text-lg hover:bg-[#6B62D0] hover:text-[#FFFFFF]">
+            <FaInbox></FaInbox> Inbox
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="text-[#A2A6C1]  p-3 bg-transparent p-0 text-lg hover:bg-[#6B62D0] hover:text-[#FFFFFF]">
+            <FaCog></FaCog> Settings
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="text-[#A2A6C1]  p-3 bg-transparent p-0 text-lg hover:bg-[#6B62D0] hover:text-[#FFFFFF]">
+            <FaBell></FaBell> Notification
+            </NavLink>
           </li>
         </ul>
       </div>

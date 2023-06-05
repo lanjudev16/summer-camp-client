e1890f1c869aa6095700bc8ccfcac8d71e557810
lanjudev16 <lanjudev16@gmail.com>
2023-06-06 03:27:20 +0600

    Nav responsive done

--- src/Pages/DashboardContent/DashNav/DashNav.jsx
+++ src/Pages/DashboardContent/DashNav/DashNav.jsx
@@ -6,13 +6,13 @@ const DashNav = () => {
     <div className="navbar bg-[#2E3248] px-6">
       <div className="navbar-start">
         <form className="flex items-center gap-2 justify-center">
-          <span>
+          <span className="lg:ml-0 ml-24">
             <FaSearch className="text-[#A2A6C1]"></FaSearch>
           </span>
           <input
             type="search"
             name=""
-            className="bg-transparent outline-none text-[#A2A6C1] text-lg"
+            className="lg:visible hidden bg-transparent outline-none text-[#A2A6C1] text-lg"
             placeholder="search"
             id=""
           />
@@ -23,10 +23,10 @@ const DashNav = () => {
           <span className="indicator-item badge badge-[#A2A6C1] bg-[#EA5455]">
             9+
           </span>
-          <FaBell className="text-[#A2A6C1] text-2xl"></FaBell>
+          <span><FaBell className="text-[#A2A6C1] text-2xl"></FaBell></span>
         </div>
         <div className="avatar online">
-          <div className="w-24 rounded-full">
+          <div className="w-24 h-[24]  rounded-full">
             <img src="https://i.ibb.co/pbw8pDN/P3-OLGJ1-copy-1-removebg-preview.png" />
           </div>
         </div>

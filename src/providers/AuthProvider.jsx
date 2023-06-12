d29fb1f3d0b5f6b46a41968fb6ddb73dc88e8b1d
lanjudev16 <lanjudev16@gmail.com>
2023-06-12 13:00:08 +0600

    jwt client create

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 
@@ -40,7 +41,14 @@ const AuthProvider = ({ children }) => {
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
-            setUser(currentUser);  
+            setUser(currentUser); 
+            if(currentUser){
+                axios.post('http://localhost:5000/jwt',{email:currentUser.email}).then(data=>{
+                    localStorage.setItem('token',data.data.token)
+                }) 
+            }else{
+                localStorage.removeItem('token')
+            }
             setLoading(false)
         });
         return () => {
